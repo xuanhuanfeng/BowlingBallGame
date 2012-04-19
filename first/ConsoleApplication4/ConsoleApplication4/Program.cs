fe2267770cb3ba4752dd17c10318a49cad59b12a
Chen Zhipeng <305867134@qq.com>
2012-04-19 22:33:46 +0800

    1402 Signed-off-by: Chen Zhipeng <305867134@qq.com>

---
+++ first/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Frame
+    {
+        private Game game = null;
+        private int lastFrame = 0;
+        public int Score { get; set; }
+
+        private bool isAll = false;
+        public bool IsAll
+        {
+            get { return isAll; }
+            set { isAll = value; }
+        }
+
+        private bool isSpare = false;
+        public bool IsSpare
+        {
+            get { return isSpare; }
+            set { isSpare = value; }
+        }
+
+        private bool isFirstThrow = true;
+
+        public Frame(Game game)
+        {
+            this.game = game;
+        }
+        public Frame()
+        {
+            this.game = null;
+        }
+
+        public void Throw(int pins)
+        {
+            if (isFirstThrow && (0 <= pins && 10 >= pins))
+            {
+                if (10 == pins)
+                    IsAll = true;
+                else
+                    isFirstThrow = false;
+                getScore(pins);
+            }
+            else if (!isFirstThrow && (0 <= pins && (10 - lastFrame) >= pins))
+            {
+                if ((10 - lastFrame) == pins)
+                    IsSpare = true;
+
+                isFirstThrow = true;
+                getScore(pins);
+            }           
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            if (IsAll)
+                IsAll = false;
+            if (IsSpare)
+                IsSpare = false;
+        }
+
+        private void getScore(int pins)
+        {
+            lastFrame = Score = pins;
+            game.Add(this);
+        }
+    }
+
+    public class Game
+    {
+        private List<Frame> frameList = new List<Frame>();
+        private int score = 0;
+        private  const int FrameNum = 21;
+
+        public int Score() 
+        {
+            score = 0;
+            for (int i = 0; i < frameList.Count; i++)
+            {
+                if(frameList[i].IsAll)
+                    score += (frameList[i].Score +
+                        frameList[i + 1].Score + frameList[i + 2].Score);
+                else if(frameList[i].IsSpare)
+                    score += (frameList[i].Score +frameList[i + 1].Score);
+                else
+                    score += frameList[i].Score;
+            }
+                return score;
+        }
+
+        public void Add(Frame frame)
+        {
+            if (frameList.Count < FrameNum)
+            {
+                Frame _frame = new Frame();
+
+                _frame.Score = frame.Score;
+                _frame.IsSpare = frame.IsSpare;
+                _frame.IsAll = frame.IsAll;
+
+                frameList.Add(_frame);
+            }
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ third/ConsoleApplication4/BowlingBallTest/BowlingTest.cs
@@ -0,0 +1,116 @@
+﻿using BowlingBall;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BowlingBallTest
+{
+
+
+    /// <summary>
+    ///这是 ProgramTest 的测试类，旨在
+    ///包含所有 ProgramTest 单元测试
+    ///</summary>
+    [TestClass()]
+    public class ProgramTest
+    {
+        private Game game = new Game();
+
+        [TestMethod]
+        public void FirstThrow()
+        {
+            game.Throw(5);
+            Assert.AreEqual(5, game.Score());
+        }
+
+        [TestMethod]
+        public void SecondThrow()
+        {
+            game.Throw(5);
+            game.Throw(3);
+            Assert.AreEqual(8, game.Score());
+        }
+
+        [TestMethod]
+        public void ThirdTurn()
+        {
+            game.Throw(1);
+            game.Throw(4);
+
+            game.Throw(4);
+            game.Throw(5);
+
+            game.Throw(5);
+            game.Throw(4);
+            Assert.AreEqual(23, game.Score());
+        }
+
+        [TestMethod]
+        public void HasSpareTurn()
+        {
+            game.Throw(1);
+            game.Throw(4);
+
+            game.Throw(5);
+            game.Throw(5);
+
+            game.Throw(6);
+            Assert.AreEqual(27, game.Score());
+        }
+
+        [TestMethod]
+        public void HasAllTurn()
+        {
+            game.Throw(1);
+            game.Throw(4);
+
+            game.Throw(4);
+            game.Throw(5);
+
+            game.Throw(6);
+            game.Throw(4);
+
+            game.Throw(5);
+            game.Throw(5);
+
+            game.Throw(10);
+
+            game.Throw(0);
+            game.Throw(1);
+            Assert.AreEqual(61, game.Score());
+        }
+
+        [TestMethod]
+        public void TheTenthTurn()
+        {
+            game.Throw(1);
+            game.Throw(4);
+
+            game.Throw(4);
+            game.Throw(5);
+
+            game.Throw(6);
+            game.Throw(4);
+
+            game.Throw(5);
+            game.Throw(5);
+
+            game.Throw(10);
+
+            game.Throw(0);
+            game.Throw(1);
+
+            game.Throw(7);
+            game.Throw(3);
+
+            game.Throw(6);
+            game.Throw(4);
+
+            game.Throw(10);
+
+            game.Throw(2);
+            game.Throw(8);
+            game.Throw(6);
+            Assert.AreEqual(133, game.Score());
+        } 
+    }
+}
